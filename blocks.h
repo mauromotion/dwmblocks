e722a1ed58132cef0e51d1e3fddd68df206f0a8a
mauromotion <hello@mauromotion.com>
2024-07-08 19:41:28 +0100

    updates script always visible

--- blocks.h
+++ blocks.h
@@ -3,7 +3,6 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
   /* {"",      "~/src/suckless/dwmblocks/scripts/player-cmus.sh", 10, 0}, */
   /* {"",      "~/src/suckless/dwmblocks/scripts/task.sh", 400, 12}, */
-  {"",    "~/src/suckless/dwmblocks/scripts/updates.sh",              360, 9},
   {"",    "~/src/suckless/dwmblocks/scripts/kernel.sh",             1000, 13},
   {"",    "~/src/suckless/dwmblocks/scripts/volume.sh",                0, 10},
   {"",    "~/src/suckless/dwmblocks/scripts/hd_space_root.sh",         30, 0},
@@ -11,6 +10,7 @@ static const Block blocks[] = {
   {"",    "~/src/suckless/dwmblocks/scripts/cpu.sh",                   3, 11},
 	{"",    "~/src/suckless/dwmblocks/scripts/memory.sh",	                6, 1},
 	{"",    "~/src/suckless/dwmblocks/scripts/clock.sh",					        5, 0},
+  {"",    "~/src/suckless/dwmblocks/scripts/updates.sh",              360, 9},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
