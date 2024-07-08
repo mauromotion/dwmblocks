9bbacbd209c992cdaec125b315a439056a57b5ca
mauromotion <hello@mauromotion.com>
2024-07-08 09:32:32 +0100

    added kernel script

--- blocks.h
+++ blocks.h
@@ -3,7 +3,8 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
   /* {"",      "~/src/suckless/dwmblocks/scripts/player-cmus.sh", 10, 0}, */
   {"",      "~/src/suckless/dwmblocks/scripts/updates.sh", 360, 9},
-  {"",      "~/src/suckless/dwmblocks/scripts/task.sh", 400, 12},
+  /* {"",      "~/src/suckless/dwmblocks/scripts/task.sh", 400, 12}, */
+  {"",      "~/src/suckless/dwmblocks/scripts/kernel.sh", 1000, 13},
   {" ",    "~/src/suckless/dwmblocks/scripts/volume.sh", 0, 10},
   {"",      "~/src/suckless/dwmblocks/scripts/hd_space_root.sh", 30, 0},
   {"",      "~/src/suckless/dwmblocks/scripts/hd_space_media.sh", 30, 0},
